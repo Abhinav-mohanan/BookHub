d138f6f175fd276ab7cbde7879eb96ee36985761
Abhinav mohanan <abhinavmohanan018@gmail.com>
2026-01-21 01:02:38 +0530

    feat(management): add pagination for list-transactions and pending-verifications

--- backend/BookHub/management/views.py
+++ backend/BookHub/management/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ValidationError
 from authentication.models import CustomUser
@@ -18,8 +19,10 @@ class AdminVerificationRequestsView(APIView):
 
     def get(self,request):
         pending_admins = CustomUser.objects.filter(role='admin', is_verified=False)
-        serializer = StaffDisplaySerializer(pending_admins, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        paginator = PageNumberPagination()
+        page = paginator.paginate_queryset(pending_admins,request)
+        serializer = StaffDisplaySerializer(page, many=True)
+        return paginator.get_paginated_response(serializer.data)
     
     def post(self,request):
         user_id = request.data.get('user_id')
@@ -49,7 +52,9 @@ class AdminTransactionUpdateView(APIView):
 
     def get(self,request):
         transactions = BorrowTransaction.objects.select_related('book','user').all().order_by('-request_at')
-        serializer = TransactionDetailSerializer(transactions,many=True)
+        paginator = PageNumberPagination()
+        page = paginator.paginate_queryset(transactions,request)
+        serializer = TransactionDetailSerializer(page,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
 
     def patch(self,request,transaction_id):
