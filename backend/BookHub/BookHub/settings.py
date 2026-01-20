d792e8ff3b42418dcfd51015b916d5d71aeeaa69
Abhinav mohanan <abhinavmohanan018@gmail.com>
2026-01-21 00:42:00 +0530

    feat:(books): implement pagination using rest_framework PageNumberPagination

--- backend/BookHub/BookHub/settings.py
+++ backend/BookHub/BookHub/settings.py
@@ -69,7 +69,12 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'authentication.authenticate.CustomJWTAuthentication',
-    )
+    ),
+    
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE':6,
+        
 }
 
 
