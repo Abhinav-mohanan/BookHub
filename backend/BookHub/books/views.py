458d83a6a960ea496cf33d912e975dd212b7d904
Abhinav mohanan <abhinavmohanan018@gmail.com>
2026-01-21 18:36:50 +0530

    feat(book): add image upload to book creation

--- backend/BookHub/books/views.py
+++ backend/BookHub/books/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.parsers import MultiPartParser, FormParser
 from django.shortcuts import get_object_or_404
 from management.permissions import IsAdmin
 from .models import Book,Category
@@ -50,16 +51,18 @@ class AdminCategoryDetailView(APIView):
 
 class AdminBookListCreateView(APIView):
     permission_classes = [IsAdmin]
-    
+    parser_classes = [MultiPartParser, FormParser]
+
     def get(self,request):
-        books = Book.objects.filter(is_delete=False)
+        books = Book.objects.filter(is_delete=False).prefetch_related('images')
         paginator = PageNumberPagination()
         page = paginator.paginate_queryset(books,request)
-        serarilzer = BookManagementSerializer(page, many=True)
-        return paginator.get_paginated_response(serarilzer.data)
+        serializer = BookManagementSerializer(page, many=True)
+        return paginator.get_paginated_response(serializer.data)
     
     def post(self,request):
         serializer = BookManagementSerializer(data=request.data)
+        print(request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
@@ -68,6 +71,7 @@ class AdminBookListCreateView(APIView):
     
 class AdminBookDetailView(APIView):
     permission_classes = [IsAdmin]
+    parser_classes = [MultiPartParser, FormParser]
 
     def put(self,request,slug):
         book = get_object_or_404(Book,slug=slug)
@@ -88,3 +92,4 @@ class AdminBookDetailView(APIView):
             {"message":f"Book {status_msg} Successfully","is_active": not book.is_delete},
             status=status.HTTP_200_OK
         )
+    
