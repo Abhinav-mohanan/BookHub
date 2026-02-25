0e837e2d483f9b90bc3d96a10581b027dd8d3eff
Abhinav mohanan <abhinavmohanan018@gmail.com>
2026-02-25 16:00:46 +0530

    chore(backend): switch Django database settings to DATABASE_URL for local and production

--- backend/BookHub/BookHub/settings.py
+++ backend/BookHub/BookHub/settings.py
@@ -13,6 +13,7 @@ import environ
 from pathlib import Path
 import os
 from datetime import timedelta
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -110,7 +111,10 @@ WSGI_APPLICATION = 'BookHub.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db('MY_PSQL_DB')  # PostgreSQL
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+    )
 }
 
 
